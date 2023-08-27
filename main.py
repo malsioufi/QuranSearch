4ba5e1d2ade26f22723153b86f5174f811734c47
Mohamad Alsioufi <malsioufy@gmail.com>
2023-08-27 11:21:51 +0200

    feat: add ayahs from quran-simple endpoint

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Document, Integer, Text
 
-base_url = "http://api.alquran.cloud/v1/page/{}/quran-uthmani"
+base_url_uthmani = "http://api.alquran.cloud/v1/page/{}/quran-uthmani"
+base_url_simple = "http://api.alquran.cloud/v1/page/{}/quran-simple"
 
 # Elasticsearch authentication credentials
 es_username = "your_username"
@@ -18,7 +19,8 @@ es = Elasticsearch(
 class Ayah(Document):
     page_number = Integer()
     ayah_number = Integer()
-    ayah_text = Text()
+    ayah_text_uthmani = Text()
+    ayah_text_simple = Text()
     surah_number = Integer()
     surah_name = Text()
 
@@ -32,26 +34,32 @@ if not es.indices.exists(index=Ayah.Index.name):
 
 # Loop through all pages (604 pages in total)
 for page_number in range(1, 605):
-    url = base_url.format(page_number)
+    url_uthmani = base_url_uthmani.format(page_number)
+    url_simple = base_url_simple.format(page_number)
 
-    response = requests.get(url)
+    response_uthmani = requests.get(url_uthmani)
+    response_simple = requests.get(url_simple)
 
-    if response.status_code == 200:
-        page_data = response.json()["data"]
+    if response_uthmani.status_code == 200 and response_simple.status_code == 200:
+        page_data_uthmani = response_uthmani.json()["data"]
+        page_data_simple = response_simple.json()["data"]
 
-        if "ayahs" in page_data:
-            ayahs = page_data["ayahs"]
+        if "ayahs" in page_data_uthmani and "ayahs" in page_data_simple:
+            ayahs_uthmani = page_data_uthmani["ayahs"]
+            ayahs_simple = page_data_simple["ayahs"]
 
-            for ayah in ayahs:
-                ayah_text = ayah["text"]
-                ayah_number = int(ayah["number"])
-                surah_number = int(ayah["surah"]["number"])
-                surah_name = ayah["surah"]["name"]
+            for ayah_uthmani, ayah_simple in zip(ayahs_uthmani, ayahs_simple):
+                ayah_number = int(ayah_uthmani["number"])
+                surah_number = int(ayah_uthmani["surah"]["number"])
+                surah_name = ayah_uthmani["surah"]["name"]
+                ayah_text_uthmani = ayah_uthmani["text"]
+                ayah_text_simple = ayah_simple["text"]
 
                 ayah_doc = Ayah(
                     page_number=page_number,
                     ayah_number=ayah_number,
-                    ayah_text=ayah_text,
+                    ayah_text_uthmani=ayah_text_uthmani,
+                    ayah_text_simple=ayah_text_simple,
                     surah_number=surah_number,
                     surah_name=surah_name,
                 )
@@ -66,5 +74,5 @@ for page_number in range(1, 605):
 
     else:
         print(
-            f"Failed to fetch page {page_number}. Status code: {response.status_code}"
+            f"Failed to fetch page {page_number}. Uthmani Status code: {response_uthmani.status_code}, Simple Status code: {response_simple.status_code}"
         )
