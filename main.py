6634ee27bfab2db3c4ba13af95313a5f47068f21
Mohamad Alsioufi <malsioufy@gmail.com>
2023-12-21 07:49:39 +0100

    imporve the code

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import requests
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Document, Integer, Text, Boolean
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+# Configure a file handler for the logger
+file_handler = logging.FileHandler('app_log.log')
+file_handler.setLevel(logging.ERROR)  # Log only errors to the file
+file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+# User-defined constants
+ELASTICSEARCH_HOST = "localhost"
+ELASTICSEARCH_PORT = 9200
+ELASTICSEARCH_USERNAME = "your_username"
+ELASTICSEARCH_PASSWORD = "your_password"
+INDEX_PREFIX = "ayahs_in_"
+MAX_PAGES = 605
 
 # Define the API URL to get the list of Arabic versions
-editions_url = "https://api.alquran.cloud/v1/edition?format=text&language=ar&type=quran"
-
-# Fetch the list of Arabic versions
-response = requests.get(editions_url)
-
-if response.status_code == 200:
-    arabic_versions = response.json()["data"]
-else:
-    print("Failed to fetch the list of Arabic versions.")
-    arabic_versions = []
-
-# Elasticsearch authentication credentials
-es_username = "your_username"
-es_password = "your_password"
+EDITIONS_URL = "https://api.alquran.cloud/v1/edition?format=text&language=ar&type=quran"
 
 # Define Elasticsearch connection with authentication
-es = Elasticsearch(
-    [{"host": "localhost", "port": 9200}], http_auth=(es_username, es_password)
+ES = Elasticsearch(
+    [{"host": ELASTICSEARCH_HOST, "port": ELASTICSEARCH_PORT}],
+    http_auth=(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD),
 )
 
 
@@ -43,65 +51,109 @@ class Ayah(Document):
         return super().init(index=index_name, using=using)
 
 
-# Function to fetch and process ayahs for a specific Arabic version
-def process_arabic_version(version):
-    # Delete the index if it exists
-    index_name = f"ayahs_in_{version['identifier']}"
-    if es.indices.exists(index=index_name):
-        es.indices.delete(index=index_name)
-        print(f"Deleted existing index: {index_name}")
-    # Create the index
-    Ayah.init(using=es, index=index_name)
-    print(f"Created index: {index_name}")
-    print(f"\t\t{version['identifier']}")
-    base_url = f"http://api.alquran.cloud/v1/page/{{}}/{version['identifier']}"
-    for page_number in range(1, 605):
-        print(f"\t\t\tPage: {page_number}")
-        url = base_url.format(page_number)
-        response = requests.get(url)
-        if response.status_code == 200:
-            page_data = response.json()["data"]
-            ayahs = page_data.get("ayahs")
-            if ayahs:
-                for ayah in ayahs:
-                    ayah_number_in_quran = int(ayah["number"])
-                    ayah_doc = Ayah(
-                        version=version,
-                        ayah_text=ayah["text"],
-                        ayah_number_in_surah=int(ayah["numberInSurah"]),
-                        ayah_number_in_quran=ayah_number_in_quran,
-                        ayah_surah_name=ayah["surah"]["name"],
-                        ayah_surah_number=int(ayah["surah"]["number"]),
-                        ayah_page_number=int(ayah["page"]),
-                        ayah_ruku_number=int(ayah["ruku"]),
-                        ayah_hizbQuarter_number=int(ayah["hizbQuarter"]),
-                        ayah_is_sajda=ayah["sajda"],
-                    )
-                    ayah_doc.meta.id = f"{ayah_number_in_quran}"
-                    ayah_doc.save()
+def fetch_arabic_versions():
+    response = requests.get(EDITIONS_URL)
+    if response.status_code == 200:
+        return response.json().get("data", [])
+    else:
+        logger.error("Failed to fetch the list of Arabic versions.")
+        return []
+
+
+def configure_logger():
+    # Set up logging
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+
+    # Configure a file handler for the logger
+    file_handler = logging.FileHandler('app_log.log')
+    file_handler.setLevel(logging.ERROR)  # Log only errors to the file
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+    return logger
+
+
+def configure_elasticsearch():
+    return Elasticsearch(
+        [{"host": ELASTICSEARCH_HOST, "port": ELASTICSEARCH_PORT}],
+        http_auth=(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD),
+    )
+
+
+def process_arabic_version(version, es):
+    try:
+        index_name = INDEX_PREFIX + version["identifier"]
+        if es.indices.exists(index=index_name):
+            es.indices.delete(index=index_name)
+            logger.info(f"Deleted existing index: {index_name}")
+        Ayah.init(using=es, index=index_name)
+        logger.info(f"Created index: {index_name}")
+
+        base_url = f"http://api.alquran.cloud/v1/page/{{}}/{version['identifier']}"
+        for page_number in range(1, MAX_PAGES + 1):
+            url = base_url.format(page_number)
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                page_data = response.json().get("data", {})
+                ayahs = page_data.get("ayahs", [])
+
+                if ayahs:
+                    bulk_data = [
+                        {
+                            "_op_type": "index",  # Action: Index a document
+                            "_index": index_name,  # Index name
+                            "_type": "_doc",
+                            "_id": str(ayah["number"]),  # Document ID
+                            "_source": {
+                                "version": version,
+                                "ayah_text": ayah["text"],
+                                "ayah_number_in_surah": int(ayah["numberInSurah"]),
+                                "ayah_number_in_quran": int(ayah["number"]),
+                                "ayah_surah_name": ayah["surah"]["name"],
+                                "ayah_surah_number": int(ayah["surah"]["number"]),
+                                "ayah_page_number": int(ayah["page"]),
+                                "ayah_ruku_number": int(ayah["ruku"]),
+                                "ayah_hizbQuarter_number": int(ayah["hizbQuarter"]),
+                                "ayah_is_sajda": ayah["sajda"],
+                            },
+                        }
+                        for ayah in ayahs
+                    ]
+                    # Log the content of bulk_data
+                    print(f"Bulk Data: {bulk_data[0]}")
+                    es.bulk(body=bulk_data)  # The bulk API request
+                else:
+                    logger.warning(f"Ayahs not found in {version['identifier']}, page {page_number} data.")
+                    logger.info(f"URL: {url}")
             else:
-                print(
-                    f"Ayahs not found in {version['identifier']}, page {page_number} data."
+                logger.error(
+                    f"Failed to fetch page {page_number} for {version['identifier']}. Status code: {response.status_code}"
                 )
-                print(f"URL: {url}")
-        else:
-            print(
-                f"Failed to fetch page {page_number} for {version['identifier']}. Status code: {response.status_code}"
-            )
-            print(f"URL: {url}")
+                logger.info(f"URL: {url}")
+                # Log the problematic URL to the file for later retry
+                file_handler.error(f"Failed to fetch page {page_number} for {version['identifier']}. Status code: {response.status_code}. URL: {url}")
+    except Exception as e:
+        logger.exception(f"An error occurred: {str(e)}")
 
 
-# Fetch and process ayahs for each Arabic version
-for version in arabic_versions:
-    process_arabic_version(version)
+def main():
+    logger = configure_logger()
+    es = configure_elasticsearch()
 
-print("Data retrieval and indexing completed.")
+    arabic_versions = fetch_arabic_versions()
+    if not arabic_versions:
+        logger.error("No Arabic versions found. Exiting.")
+        return
 
-# Credits to the API provider
-print("Data provided by AlQuran Cloud API (http://api.alquran.cloud)")
+    for version in arabic_versions:
+        process_arabic_version(version, es)
 
-# Credits to OpenAI assistant for code assistance and User for collaboration
-print(
-    "Code developed with assistance from OpenAI's GPT-3 assistant and in collaboration "
-    "with Mohamad Alsioufi"
-)
+    logger.info("Data retrieval and indexing completed.")
+    logger.info("Data provided by AlQuran Cloud API (http://api.alquran.cloud)")
+    logger.info("Code developed with assistance from OpenAI's GPT-3 assistant and in collaboration with Mohamad Alsioufi")
+
+
+if __name__ == "__main__":
+    main()
