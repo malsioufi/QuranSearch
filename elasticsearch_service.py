b85a40bbde3ed3f69c02c0cb95b46817eb7dafa8
Mohamad Alsioufi <malsioufy@gmail.com>
2023-12-21 08:24:15 +0100

    Introduce elasticsearch_service module for Elasticsearch-related operations

---
+++ elasticsearch_service.py
@@ -0,0 +1,30 @@
+# elasticsearch_service.py
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Document, Integer, Text, Boolean
+from config import ELASTICSEARCH_HOST, ELASTICSEARCH_PORT, ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD
+
+
+# Define Elasticsearch index and mapping
+class Ayah(Document):
+    version = Text(analyzer="keyword")
+    ayah_text = Text(required=True)
+    ayah_number_in_surah = Integer()
+    ayah_number_in_quran = Integer()
+    ayah_surah_name = Text(analyzer="keyword")
+    ayah_surah_number = Integer()
+    ayah_page_number = Integer()
+    ayah_ruku_number = Integer()
+    ayah_hizbQuarter_number = Integer()
+    ayah_is_sajda = Boolean()
+
+    @classmethod
+    def init(cls, index=None, using=None):
+        index_name = index or "quran_ayahs"
+        return super().init(index=index_name, using=using)
+
+
+def configure_elasticsearch():
+    return Elasticsearch(
+        [{"host": ELASTICSEARCH_HOST, "port": ELASTICSEARCH_PORT}],
+        http_auth=(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD),
+    )
--- main.py
+++ main.py
@@ -1,58 +1,10 @@
 import requests
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Document, Integer, Text, Boolean
-import logging
-from config import ELASTICSEARCH_HOST, ELASTICSEARCH_PORT, ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD, INDEX_PREFIX, MAX_PAGES, EDITIONS_URL
+from config import INDEX_PREFIX, MAX_PAGES
 from logger import configure_logger
 from api import fetch_arabic_versions
+from elasticsearch_service import configure_elasticsearch, Ayah
 
 logger = configure_logger()
-
-# Define Elasticsearch connection with authentication
-ES = Elasticsearch(
-    [{"host": ELASTICSEARCH_HOST, "port": ELASTICSEARCH_PORT}],
-    http_auth=(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD),
-)
-
-
-# Define Elasticsearch index and mapping
-class Ayah(Document):
-    version = Text(analyzer="keyword")
-    ayah_text = Text(required=True)
-    ayah_number_in_surah = Integer()
-    ayah_number_in_quran = Integer()
-    ayah_surah_name = Text(analyzer="keyword")
-    ayah_surah_number = Integer()
-    ayah_page_number = Integer()
-    ayah_ruku_number = Integer()
-    ayah_hizbQuarter_number = Integer()
-    ayah_is_sajda = Boolean()
-
-    @classmethod
-    def init(cls, index=None, using=None):
-        index_name = index or "quran_ayahs"
-        return super().init(index=index_name, using=using)
-
-
-def configure_logger():
-    # Set up logging
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger(__name__)
-
-    # Configure a file handler for the logger
-    file_handler = logging.FileHandler('app_log.log')
-    file_handler.setLevel(logging.ERROR)  # Log only errors to the file
-    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(file_formatter)
-    logger.addHandler(file_handler)
-    return logger
-
-
-def configure_elasticsearch():
-    return Elasticsearch(
-        [{"host": ELASTICSEARCH_HOST, "port": ELASTICSEARCH_PORT}],
-        http_auth=(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD),
-    )
 
 
 def process_arabic_version(version, es):
@@ -99,7 +51,9 @@ def process_arabic_version(version, es):
                     print(f"Bulk Data: {bulk_data[0]}")
                     es.bulk(body=bulk_data)  # The bulk API request
                 else:
-                    logger.warning(f"Ayahs not found in {version['identifier']}, page {page_number} data.")
+                    logger.warning(
+                        f"Ayahs not found in {version['identifier']}, page {page_number} data."
+                    )
                     logger.info(f"URL: {url}")
             else:
                 logger.error(
@@ -107,7 +61,9 @@ def process_arabic_version(version, es):
                 )
                 logger.info(f"URL: {url}")
                 # Log the problematic URL to the file for later retry
-                file_handler.error(f"Failed to fetch page {page_number} for {version['identifier']}. Status code: {response.status_code}. URL: {url}")
+                file_handler.error(
+                    f"Failed to fetch page {page_number} for {version['identifier']}. Status code: {response.status_code}. URL: {url}"
+                )
     except Exception as e:
         logger.exception(f"An error occurred: {str(e)}")
 
@@ -126,7 +82,9 @@ def main():
 
     logger.info("Data retrieval and indexing completed.")
     logger.info("Data provided by AlQuran Cloud API (http://api.alquran.cloud)")
-    logger.info("Code developed with assistance from OpenAI's GPT-3 assistant and in collaboration with Mohamad Alsioufi")
+    logger.info(
+        "Code developed with assistance from OpenAI's GPT-3 assistant and in collaboration with Mohamad Alsioufi"
+    )
 
 
 if __name__ == "__main__":
