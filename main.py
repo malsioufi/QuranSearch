2f084c205a097475acc141c80cdf94eeb8ec7435
Mohamad Alsioufi <malsioufy@gmail.com>
2023-12-24 08:57:05 +0100

    introduce rerun mode in main

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+import argparse
+import sys
+from data_processing import process_edition
 from logger import logger
 from quran_api import fetch_arabic_editions
-from elasticsearch_service import configure_elasticsearch
-from data_processing import process_arabic_edition
 
 
 def main():
-    es = configure_elasticsearch()
+    parser = argparse.ArgumentParser(description="Description of your script.")
+    parser.add_argument(
+        "--rerun",
+        action="store_true",
+        help="Rerun mode to process failed links from the log file.",
+    )
+    parser.add_argument(
+        "--links_list",
+        type=str,
+        required="--rerun" in sys.argv,
+        help="Path to the log file.",
+    )
+    args = parser.parse_args()
 
-    arabic_editions = fetch_arabic_editions()
+    if args.rerun:
+        # Rerun mode
+        # rerun_failed_links(args.links_list)
+        pass
 
-    if not arabic_editions:
-        logger.error("No Arabic versions found. Exiting.")
-        return
+    else:
+        arabic_editions = fetch_arabic_editions()
 
-    for edition in arabic_editions:
-        process_arabic_edition(edition, es)
+        if not arabic_editions:
+            logger.error("No Arabic versions found. Exiting.")
+            return
+        # Normal mode
+        for arabic_edition in arabic_editions:
+            process_edition(edition=arabic_edition)
 
     logger.info("Data retrieval and indexing completed.")
     logger.info("Data provided by AlQuran Cloud API (http://api.alquran.cloud)")
